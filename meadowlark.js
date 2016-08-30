b4c0fb68f170496d881ee33f8fac153b6159571b
Roma Bzoviy <tappochek@gmail.com>
2016-08-31 00:02:22 +0300

    add testing

--- meadowlark.js
+++ meadowlark.js
@@ -1,24 +1,53 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
+
 var app = express();
 
-var fortune = require('./lib/fortune');
-
 // Установка механизма представления handlebars
-var handlebars = require('express-handlebars')
-    .create({defaultLayout: 'main'});
+var handlebars = require('express3-handlebars').create({
+    defaultLayout: 'main',
+    helpers: {
+        section: function (name, options) {
+            if (!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.set('port', process.env.PORT || 3000);
+
 app.use(express.static(__dirname + '/public'));
 
-app.set('port', process.env.PORT || 3000);
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
 
 app.get('/', function (req, res) {
     res.render('home');
 });
 
 app.get('/about', function (req, res) {
-    res.render('about', {fortune: fortune.getFortune()});
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
+});
+
+app.get('/tours/hood-river', function (req, res) {
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/oregon-coast', function (req, res) {
+    res.render('tours/oregon-coast');
+});
+
+app.get('/tours/request-group-rate', function (req, res) {
+    res.render('tours/request-group-rate');
 });
 
 // пользовательская страница 404
@@ -36,13 +65,5 @@ app.use(function (err, req, res, next) {
 
 app.listen(app.get('port'), function () {
     console.log('Express запущен на http://localhost:' +
-    app.get('port') + '; нажмите Ctrl+C для завершения.');
+        app.get('port') + '; нажмите Ctrl+C для завершения.');
 });
-
-var fortunes = [
-    "Победи свои страхи, или они победят тебя.",
-    "Рекам нужны истоки.",
-    "Не бойся неведомого.",
-    "Тебя ждет приятный сюрприз.",
-    "Будь проще везде, где только можно."
-];
